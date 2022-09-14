1a82eaf8867929b3a628753bbbf59046c9136211
Destinyuc21 <destinyuche384@gmail.com>
2022-09-14 03:07:05 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char d = 'a'
+
+	while (d <= 'z')
+	{
+		_putchar(d);
+		d++
+	}
+	_putchar('\n');
+
+}
